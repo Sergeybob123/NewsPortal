b495e021aa0a6f957dd2d89ccd4ee6ed0baa92e7
Sergey Babchenkov <hotsalami@yandex.ru>
2023-12-06 22:17:23 +0300

    Add files via upload

--- NewsPortal/news_portal/views.py
+++ NewsPortal/news_portal/views.py
@@ -8,6 +8,7 @@ class NewsList(ListView):
     model = Post
     template_name = 'post_list.html'
     context_object_name = 'posts'
+    ordering = '-time_in'
 
 class NewsDetail(DetailView):
     model = Post
