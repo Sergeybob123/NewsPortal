cea822409d78ecc4296dcc0129ea3456c813e735
Sergey Babchenkov <hotsalami@yandex.ru>
2023-12-12 21:54:36 +0300

    Add files via upload

--- NewsPortal/NewsPortal/urls.py
+++ NewsPortal/NewsPortal/urls.py
@@ -1,23 +1,15 @@
-"""
-URL configuration for NewsPortal project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+
+from .views import *
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('news/', include('news_portal.urls')),
+    path('news/', NewsList.as_view(), name='post_list'),
+    path('news/<int:pk>/', NewsDetail.as.view(), name='post_detail'),
+    path('news/create/', NewsCreate.as_view(), name='post_search'),
+    path('news/<int:pk>/edit/', NewsUpdate.as_view(), name='post_search'),
+    path('news/<int:pk>/delete'), NewsDelete.as_view(), name='post_search'),
+    path('articles/create/', NewsSearch.as_view(), name='post_search'),
+    path('articles/<int:pk>/edit', NewsSearch.as_view(), name='post_search'),
+    path('articles/<int:pk>/delete/', NewsSearch.as_view(), name='post_search')
 ]
--- NewsPortal/news_portal/views.py
+++ NewsPortal/news_portal/views.py
@@ -9,8 +9,26 @@ class NewsList(ListView):
     template_name = 'post_list.html'
     context_object_name = 'posts'
     ordering = '-time_in'
+    paginate_by = 5
 
 class NewsDetail(DetailView):
     model = Post
     template_name = 'post_detail.html'
     context_object_name = 'post'
+
+class NewsCreate(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'post_create.html'
+    context_object_name = 'posts'
+
+class NewsUpdate(UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'post_edit.html'
+    context_object_name = 'posts'
+
+class NewsDelete(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    context_object_name = 'posts'
